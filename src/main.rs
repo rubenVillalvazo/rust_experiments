5b94d7c141d789e9d97bcd270cb9f984a4cf277a
Ruben Villalvazo <vim.ruben@hotmail.com>
2023-01-01 01:08:43 -0300

    Add random gess number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number: i32 = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
 
     let mut guess: String = String::new();
